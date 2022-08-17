323487b17ef3caa55de1fad993febfcd36a7bd86
Kade Hess <kadeahess@gmail.com>
2022-08-17 09:08:59 -0700

    commit tree 5bbb914b156e4213f9152c8291cd608cf5413051

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ['Charlie', 'Samip', 'Ali'];
+
+function writeCards(names, event) {
+  let birthday =[];
+  event = "surprise";
+  for (let i= 0; i < names.length; i++) {
+  birthday[i] = 'Thank you, '+ names[i] +', for the wonderful ' +event+' gift!';
+  }
+  return birthday;
+}
+var countDown = function(value) {
+  if (value >= 0) {
+      console.log(value);
+      return countDown(value - 1);
+  } else {
+      return value;
+  }
+};
+countDown(10);
